498bc49420afc0e4a355f33a8a516cdf15a2d5fd
gao <13644805888@163.com>
2023-06-05 17:13:44 +0800

    v 0.0.4

--- go_file/src/file-transfer-system/client/main.go
+++ go_file/src/file-transfer-system/client/main.go
@@ -8,52 +8,54 @@ import (
 	"os"
 )
 
-var path string ="/home/gao/test/dome/learning_log/go_file/src/file-transfer-system/client/a.txt"
+const filePath = "a.txt"
+
 func main() {
-	file, err := os.Open(path)
+	file, err := os.Open(filePath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error opening file: %v", err)
 	}
 	defer file.Close()
+
 	fileInfo, err := file.Stat()
-	fmt.Println(fileInfo.Name(),fileInfo.Size())
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-	conn, err := net.Dial("tcp", "127.0.0.1:8888")
 	if err != nil {
-		fmt.Println("Error connecting:", err)
-		return
+		log.Fatalf("Error getting file info: %v", err)
 	}
-	defer conn.Close()
+	connection, err := net.Dial("tcp", "127.0.0.1:8888")
+
+	if err != nil {
+		log.Fatalf("Error connecting to server: %v", err)
+	}
+	defer connection.Close()
+
 	fileName := fileInfo.Name()
 	fileSize := fileInfo.Size()
-    if _, err := fmt.Fprintln(conn, fileName,fileSize); err != nil {
-        fmt.Println(err)
-        return
-    }
+
+	if _, err := fmt.Fprintf(connection, "%s %d\n", fileName, fileSize); err != nil {
+		log.Fatalf("Error sending file info: %v", err)
+	}
 	fmt.Printf("开始上传文件 %s...\n", fileName)
 	buffer := make([]byte, 1024)
 	for {
-        n, err := file.Read(buffer)
-        if err != nil && err != io.EOF {
-            log.Fatal(err)
-        }
-        if n == 0 {
-            break
-        }
-		if _, err := fmt.Fprintln(conn, n); err != nil {
-			fmt.Println(err)
+		bytesRead, err := file.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+				return
+			}
+			break
+		}
+		if _, err := fmt.Fprintln(connection, bytesRead); err != nil {
+			log.Fatal(err)
 			return
 		}
-		if _, err := conn.Write(buffer[:n]); err != nil {
-            fmt.Println(err)
-            return
-        }
-    }
-	var xc []byte=make([]byte, 1024)
-	c,_:=conn.Read(xc)
-	fmt.Println(string(xc[:c]))
+		if _, err := connection.Write(buffer[:bytesRead]); err != nil {
+			log.Fatalf("Error sending file data: %v", err)
+		}
+	}
+
+	responseBuffer := make([]byte, 1024)
+	bytesReceived, _ := connection.Read(responseBuffer)
+	fmt.Println(string(responseBuffer[:bytesReceived]))
 
 }
